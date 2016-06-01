c8a4d03a3a5c4027fffc928ca5989199531e3b42
Janet Mohler <janetmohler82@gmail.com>
2016-06-01 02:20:11 +0000

    Added a model that will capture photos for the places

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,7 @@
 class Place < ActiveRecord::Base
   belongs_to :user
   has_many :comments
+  has_many :photos
   
   validates :name, presence: true, length: { maximum: 50, minimum: 3}
   validates :address, presence: true
