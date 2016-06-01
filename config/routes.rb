25465b9fd962bb0324644bf03218c52a6dff8590
Janet Mohler <janetmohler82@gmail.com>
2016-06-01 03:31:04 +0000

    Added a photos controller with a create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Nomster::Application.routes.draw do
   devise_for :users
+
   root 'places#index'
+
   resources :places do
     resources :comments, only: :create
+  end
+  
+  resources :places do
+    resources :photos, only: :create
   end
   
 end
