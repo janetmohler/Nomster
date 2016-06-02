8bb163af2a41c919051fbd8bb2bd3dc7a885f54b
Janet Mohler <janetmohler82@gmail.com>
2016-06-02 03:45:04 +0000

    Added figaro and hooked the Place model to include a latitude and longitude field

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,9 @@ class Place < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   has_many :photos
+
+  geocoded_by :address
+  after_validation :geocode
   
   validates :name, presence: true, length: { maximum: 50, minimum: 3}
   validates :address, presence: true
